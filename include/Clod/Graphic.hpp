defd768c39cbd9a176bfdab089fedf66c2ddfb5e
chapterjason <jason@sourecode.dev>
2024-02-22 22:54:02 +0100

    Add missing includes

--- include/Clod/Graphic.hpp
+++ include/Clod/Graphic.hpp
@@ -4,3 +4,6 @@
 #include <Clod/Graphic/ImageResourceManager.hpp>
 #include <Clod/Graphic/TextureResourceManager.hpp>
 #include <Clod/Graphic/FontResourceManager.hpp>
+#include <Clod/Graphic/SoundBufferResourceManager.hpp>
+#include <Clod/Graphic/SoundResourceManager.hpp>
+#include <Clod/Graphic/MusicResourceManager.hpp>
